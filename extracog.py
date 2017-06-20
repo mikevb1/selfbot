a96e0286914f4bdb5055a1817c7e5d8bcb84207c
Michael Van Buren <mike64221@gmail.com>
2017-06-20 11:41:55 -0700

    add color command and find shared guilds

--- extracog.py
+++ extracog.py
@@ -13,6 +13,29 @@ import dice
 UNIURL = "http://www.fileformat.info/info/unicode/char/{}/index.htm"
 
 
+def hex_or_rgb(arg):
+    s = arg.split(' ')
+    if len(s) == 1:
+        color = s[0]
+        if len(color) == 6:
+            color = f'0x{color}'
+        elif len(color) == 7:
+            color = color.replace('#', '0x')
+        try:
+            return discord.Color(int(color, 0))
+        except ValueError:
+            raise commands.BadArgument('A single argument must be passed as hex (`0x7289DA`, `#7289DA`, `7289DA`)')
+    elif len(s) == 3:
+        try:
+            rgb = [*map(int, s)]
+        except ValueError:
+            raise commands.BadArgument('Three arguments must be passed as RGB (`114 137 218`, `153 170 181`)')
+        if any(c < 0 or c > 255 for c in rgb):
+            raise commands.BadArgument('RGB colors must be in the range `[0, 255]`')
+        return discord.Color.from_rgb(*rgb)
+    raise commands.BadArgument('You must pass 1 (hex) or 3 (RGB) arguments.')
+
+
 class Extra:
     def __init__(self, bot):
         self.bot = bot
@@ -77,13 +100,18 @@ class Extra:
             short = len(uc) <= 4
             code = f"\\{'u' if short else 'U'}{uc.lower().zfill(4 if short else 8)}"
             embed.add_field(name=name, value=f'{char} [{code}]({UNIURL.format(uc)})')
-        await ctx.message.delete()
-        await ctx.send(embed=embed)
+        await ctx.message.edit(content='', embed=embed)
 
     @commands.command()
     async def fw(self, ctx, *, chars):
         """Make full-width meme text."""
         await ctx.message.edit(content=zenhan.h2z(chars))
+
+    @commands.command()
+    async def color(self, ctx, *, color: hex_or_rgb):
+        """Show color from hex or RGB."""
+        em = discord.Embed(color=color, description=f'Hex: {color}\nRGB: {color.to_rgb()}')
+        await ctx.message.edit(content='', embed=em)
 
     @commands.command()
     async def team(self, ctx, members=0, teams=2, *exclude):
@@ -94,7 +122,6 @@ class Extra:
         [teams] = number of teams
         [exclude] = space-separated list of member ids to exclude
         """
-        await ctx.message.delete()
         names = [m.mention for m in ctx.message.author.voice.channel.members
                  if m.id not in exclude]
         random.shuffle(names)
@@ -103,7 +130,18 @@ class Extra:
         for team in range(teams):
             embed.add_field(name=f'Team {team + 1}',
                             value='\n'.join(names[team::teams]) or 'None')
-        await ctx.send(embed=embed)
+        await ctx.message.edit(content='', embed=embed)
+
+    @commands.command()
+    async def shared(self, ctx, member: discord.Member = None):
+        if member is None:
+            async for m in ctx.history():
+                if m.author != ctx.author:
+                    member = m.author
+                    break
+        mid = member.id
+        guilds = [g.name for g in self.bot.guilds if g.get_member(mid)]
+        await ctx.message.edit(content=', '.join(f'"{guild}"' for guild in guilds))
 
 
 def setup(bot):
